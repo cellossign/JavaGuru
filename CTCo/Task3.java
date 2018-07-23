1c531c0040bbb7aca8531544de340c6a39dffcea
cellosign <cellosign@gmail.com>
2018-07-23 12:24:44 +0300

    add

--- CTCo/Task3.java
+++ CTCo/Task3.java
@@ -2,7 +2,9 @@ package CTCo;
 
 public class Task3 {
     public static void main(String[] args) {
-        char[] a = new char[]{};
+        char[] a = new char[]{'(', ')', '[', ']', '{', '}'};
+        for (int i = 0; i < a.length; i++) {
 
+        }
     }
 }
